287eb893203a3d7b7f3de2a3ff5c23d7fd41284b
Stergiani Kakarali <152175194+StellaKakarali@users.noreply.github.com>
2024-02-04 18:05:43 +0100

    finish task

--- js/index.js
+++ js/index.js
@@ -24,10 +24,36 @@ const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
   // Code here
+  let currentTime = new Date().getHours();
+  if (currentTime >= 6 && currentTime <= 12) {
+    return "Good Morning";
+  }
+  if (currentTime >= 13 && currentTime <= 18) {
+    return "Good Afternoon";
+  }
+  if (currentTime >= 19 && currentTime <= 22) {
+    return "Good Evening";
+  }
+  if (currentTime >= 23 && currentTime <= 5) {
+    return "Good Night";
+  }
 }
 
 function getDayColor() {
   // Code here
+  let currentWeekDay = new Date().getDay();
+  if (currentWeekDay === 1) {
+    return "darkgrey";
+  }
+  if (currentWeekDay >= 2 && currentWeekDay <= 5) {
+    return "lightblue";
+  }
+  if (currentWeekDay >= 60) {
+    return "hotpink";
+  }
+  if (currentWeekDay === 0) {
+    return "hotpink";
+  }
 }
 
 display.textContent = getGreeting();
